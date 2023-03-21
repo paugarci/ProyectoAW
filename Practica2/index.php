bf8501c349e7dc4e346ab51d617b3eaba9d4a1fa
Hugo Silva <hsilva@ucm.es>
2023-03-21 14:48:30 +0100

    fixed merge problems and footer

--- Practica2/index.php
+++ Practica2/index.php
@@ -1,20 +1,9 @@
 <?php require "includes/comun/header.php" ?>
-
-<html>
-    <head>
-        <title>Responsive</title>
-        <meta charset="utf-8"/>
-        <link rel="stylesheet" type="text/css" href="CajasFlex.css" />		
-    </head>
-    <body>
-        <header>Esta es la cabecera</header>
-        <div id="principal">
-            <!--<nav>Barra de navegación</nav> -->
-            <article>Contenido</article>
-            <aside>Barra lateral</aside> 
-        </div>
-        <footer>Este es el pie de página</footer>
-    </body>
-</html>
-
+<div class="text-center m-5">
+    <h1>Hola</h1>
+    <h1>Esto</h1>
+    <h1>Es</h1>
+    <h1>Una</h1>
+    <h1>Portada</h1>
+</div>
 <?php require "includes/comun/footer.php" ?>
