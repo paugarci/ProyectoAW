a365d61c843c554b6f13afc3e65b70b29284ca07
Alex <alex.lopez.ve@gmail.com>
2023-03-28 16:06:16 +0200

    Reworked events. Added subpaging for events. Added template for events.

--- Practica2/eventos.php
+++ Practica2/eventos.php
@@ -1,8 +1,37 @@
 <?php
-require "includes/comun/header.php";
+    require "includes/comun/header.php";
 
-echo <<<EOS
+    $basename = basename($_SERVER['PHP_SELF']);
+    $subpageKey = 'subpage';
+    $subpageValue = 'eventos_disponibles';
 
-EOS;
+    if (isset($_GET[$subpageKey]))
+        $subpageValue = $_GET[$subpageKey];
+?>
 
-require "includes/comun/footer.php";
+<div class="wrapper d-flex align-items-stretch object-fit-fill">
+    <div class="ps-5 py-5 bg-dark" style="width: 350px"> <!-- No me mates, Hugo, jejeje -->
+    <form action="<?=$basename?>">
+            <ul class="list-unstyled">
+                <li class="active">
+                    <button type="submit" class="btn btn-link text-decoration-none text-primary" name="<?=$subpageKey?>" value="eventos_disponibles">Eventos disponibles</button>
+                </li>
+                <li class="mt-2">
+                    <button type="submit" class="btn btn-link text-decoration-none text-primary" name="<?=$subpageKey?>" value="mis_eventos">Mis eventos</button>
+                </li>
+                <li class="mt-2">
+                    <button type="submit" class="btn btn-link text-decoration-none text-primary" name="<?=$subpageKey?>" value="mi_equipo">Mi equipo</button>
+                </li>
+            </ul>
+        </div>
+        </form>
+    <div class="w-100 px-5 py-4">
+        <?php
+            require "eventos/{$subpageValue}.php"
+        ?>
+    </div>
+</div>
+
+<?php
+    require "includes/comun/footer.php";
+?>
